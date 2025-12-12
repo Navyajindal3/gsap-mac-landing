8c745c832500b6c736e7ea071e52e569e81d06cc
Navyajindal3 <navyajindal3107@gmail.com>
2025-12-12 22:35:32 +0530

    Highlights Section

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import { ScrollTrigger , SplitText} from 'gsap/all';
 import Showcase from './components/Showcase';
 import Performance from './components/Performance';
 import Features from './components/Features';
+import Highlights from './components/Highlights';
 import Footer from './components/Footer';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
       <Showcase/>
       <Performance/>
       <Features/>
+      <Highlights/>
       <Footer/>
     </main>
   )
